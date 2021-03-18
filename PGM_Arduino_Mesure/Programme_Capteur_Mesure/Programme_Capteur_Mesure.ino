aed33d558a304373dfc9ece97c6a85199b16f352
Gaich-Eleonore <gaich@etud.insa-toulouse.fr>
2021-03-18 14:57:31 +0100

    Erreur_bluetooth_ok

--- PGM_Arduino_Mesure/Programme_Capteur_Mesure/Programme_Capteur_Mesure.ino
+++ PGM_Arduino_Mesure/Programme_Capteur_Mesure/Programme_Capteur_Mesure.ino
@@ -1,12 +1,19 @@
+// Variables pour éviter le delay 
+unsigned long previousMillis = 0;        // will store last time LED was updated
+
+// constants won't change:
+const long interval = 500;           // interval at which to blink (milliseconds)
+
+
 
 //Bluetooth
 #include <SoftwareSerial.h>
-#define rxPin 6 
-#define txPin 7 
-#define baudrate 9600
+#define rxPin 6 // Correspondant à la broche tx du module bluetooth
+#define txPin 7 // Correspondant à la broche Rx du module bluetooth
+#define baudrate 9600 
 SoftwareSerial mySerial(rxPin ,txPin); //Definition du software serial
 
-//Encodeur rotatoire
+/*//Encodeur rotatoire
 #define encoder0PinA  2  //CLK Output A Do not use other pin for clock as we are using interrupt
 #define encoder0PinB  4  //DT Output B
 #define Switch 5 // Switch connection if available
@@ -26,14 +33,14 @@ SoftwareSerial mySerial(rxPin ,txPin); //Definition du software serial
 
 int OLEDPinSCL = 4;
 int OLEDPinSDA = 5;
-
+*/
 
 int capteurgraphitePin = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(OLEDPinSCL, OUTPUT);
-  pinMode(OLEDPinSDA, OUTPUT);
+ /* pinMode(OLEDPinSCL, OUTPUT);
+  pinMode(OLEDPinSDA, OUTPUT);*/
   pinMode(rxPin,INPUT);
   pinMode(txPin,OUTPUT); 
   mySerial.begin(baudrate);
@@ -67,14 +74,21 @@ float getVoltage(int pin){
   return (analogRead(pin)*(5/1024.0)); //converting from a 0 to 1023 digital range
                                         // to 0 to 5 volts (each 1 reading equals ~ 5 millivolts
 }
+
+
   
 void loop() {
+  unsigned long currentMillis = millis ();
   
-  float voltage = getVoltage(capteurgraphitePin);
-  String reading = String(voltage, 5);
-  Serial.println(reading);
-  mySerial.println(reading);
-  delay(100);
+   if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    float voltage = getVoltage(capteurgraphitePin);
+    String reading = String(voltage, 5);
+    Serial.println(reading);
+    mySerial.println(reading);
+  }
+  /*delay(250);*/
   /*Serial.print("Position:");
   Serial.println (encoder0Pos, DEC);  //Angle = (360 / Encoder_Resolution) * encoder0Pos*/
 }
